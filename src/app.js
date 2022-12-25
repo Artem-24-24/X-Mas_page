a27bc77d0d0dd9c25809a70d780ff414a380bd65
Alex <userok.zmina@gmail.com>
2022-12-25 15:08:27 +0200

    Add zoom and move actions

--- src/app.js
+++ src/app.js
@@ -2,12 +2,21 @@ import * as THREE from 'three';
 import {VRButton} from "three/addons/webxr/VRButton";
 import {BoxLineGeometry} from "three/addons/geometries/BoxLineGeometry";
 
+import {GLTFLoader} from "three/addons/loaders/GLTFLoader";
+import {DRACOLoader} from "three/addons/loaders/DRACOLoader";
+
+import snowman from "../assets/snowman_-_low_poly.glb"
+import {XRControllerModelFactory} from "three/addons/webxr/XRControllerModelFactory";
+
+const snowmanPosition = {x: 0, y: 0.5, z: -1, scale: 1.0}
+
 class App {
     constructor() {
         const container = document.createElement('div');
         document.body.appendChild(container);
 
         this.clock = new THREE.Clock();
+        this.counter = 0;
 
         this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
         this.camera.position.set(0, 1.6, 0);
@@ -48,7 +57,7 @@ class App {
             new THREE.LineBasicMaterial({color: 0x808080})
         );
 
-        const geo1 = new THREE.SphereBufferGeometry(0.1, 16, 8);
+        const geo1 = new THREE.SphereGeometry(0.1, 16, 8);
         const mat1 = new THREE.MeshStandardMaterial({color: 0x3333ff});
         const mat2 = new THREE.MeshStandardMaterial({color: 0x33ff33});
         this.materials = [mat1, mat2];
@@ -61,11 +70,59 @@ class App {
 
         this.room.geometry.translate(0, 3, 0);
         this.scene.add(this.room);
+
+        const self = this
+
+        this.loadAsset(snowman, snowmanPosition.x, snowmanPosition.y, snowmanPosition.z, scene => {
+            const scale = snowmanPosition.scale
+            scene.scale.set(scale, scale, scale)
+            self.snowman = scene
+        })
+    }
+
+    loadAsset(gltfFilename, x, y, z, sceneHandler) {
+        const self = this
+        const loader = new GLTFLoader()
+        // Provide a DRACOLoader instance to decode compressed mesh data
+        const draco = new DRACOLoader()
+        draco.setDecoderPath('draco/')
+        loader.setDRACOLoader(draco)
+
+        loader.load(gltfFilename, (gltf) => {
+                const gltfScene = gltf.scene
+                self.scene.add(gltfScene)
+                gltfScene.position.set(x, y, z)
+                if (sceneHandler) {
+                    sceneHandler(gltfScene)
+                }
+            },
+            null,
+            (error) => console.error(`An error happened: ${error}`)
+        )
     }
 
     setupXR() {
         this.renderer.xr.enabled = true;
 
+        // Add grip controllers
+        const gripRight = this.renderer.xr.getControllerGrip(0)
+        gripRight.add(new XRControllerModelFactory().createControllerModel(gripRight))
+        this.scene.add(gripRight)
+
+        const gripLeft = this.renderer.xr.getControllerGrip(1)
+        gripLeft.add(new XRControllerModelFactory().createControllerModel(gripLeft))
+        this.scene.add(gripLeft)
+
+        // Add events
+        const self = this
+        gripRight.addEventListener('squeezestart', () => {
+            // Reset position
+            const scale = snowmanPosition.scale
+            self.snowman.scale.set(scale, scale, scale)
+            self.snowman.position.set(snowmanPosition.x, snowmanPosition.y, snowmanPosition.z)
+        })
+
+        // Add Enter WebXR button
         document.body.appendChild(VRButton.createButton(this.renderer));
     }
 
@@ -84,6 +141,8 @@ class App {
             if (this.counter > .2) {
                 this.getInputSources = true
                 this.counter = 0
+                // this.rightStick(1, -1, true)   // For DEBUG
+                this.leftStick(1, 1, true)   // For DEBUG
             }
             if (this.getInputSources) {
                 const info = [];
@@ -117,17 +176,47 @@ class App {
                     const btnIndex = (thumbstick) ? 3 : 2;
                     const btnPressed = gp.buttons[btnIndex].pressed;
                     const material = (btnPressed) ? this.materials[1] : this.materials[0];
+                    const deltaX = gp.axes[offset]
+                    const deltaY = gp.axes[offset + 1]
                     if (inputSource.handedness === 'right') {
-                        this.rsphere.position.set(0.5, 1.6, -1).add(this.vec3.set(gp.axes[offset], -gp.axes[offset + 1], 0));
+                        this.rsphere.position.set(0.5, 1.6, -1).add(this.vec3.set(deltaX, -deltaY, 0));
                         this.rsphere.material = material;
+                        this.rightStick(deltaX, deltaY, btnPressed)
                     } else if (inputSource.handedness === 'left') {
-                        this.lsphere.position.set(-0.5, 1.6, -1).add(this.vec3.set(gp.axes[offset], -gp.axes[offset + 1], 0));
+                        this.lsphere.position.set(-0.5, 1.6, -1).add(this.vec3.set(deltaX, -deltaY, 0));
                         this.lsphere.material = material;
+                        this.leftStick(deltaX, deltaY, btnPressed)
                     }
                 })
             }
         }
         this.renderer.render(this.scene, this.camera);
+    }
+
+    rightStick(deltaX, deltaY, buttonPressed) {
+        if (this.snowman && buttonPressed) {
+            // Zoom model
+            const currentScale = this.snowman.scale.x
+            let scale
+            if (currentScale >= 1) {
+                scale = currentScale - .1 * deltaY
+            } else {
+                scale = 1 / (1 / currentScale + .1 * deltaY)
+            }
+            this.snowman.scale.set(scale, scale, scale)
+        } else if(this.snowman) {
+            // Rotate model
+            this.snowman.rotateY(Math.PI / 180 * 10 * deltaX)
+            this.snowman.rotateZ(Math.PI / 180 * 10 * deltaY)
+        }
+    }
+
+    leftStick(deltaX, deltaY, buttonPressed) {
+        if (this.snowman && buttonPressed) {
+
+        } else if(this.snowman) {
+            this.snowman.position.add(this.vec3.set(.05 * deltaX, 0, .05 * deltaY))
+        }
     }
 }
 
