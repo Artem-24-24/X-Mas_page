e41d06780fba942298d864b6ce20b10efe8671bd
Учень12 <artems555666777@gmail.com>
2023-04-29 17:00:04 +0300

    start page 1.1

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,39 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest')
+
+const config = {
+  entry: {
+    app: './src/index.js',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'VR Session Input',
       favicon: './assets/logo.png'
+    }),
+
+    new WebpackPwaManifest({
+      name: 'WebXR PWA Example',
+      short_name: 'WebXRPWA',
+      description: 'WebXR PWA Example for factory environment',
+      // start_url: ".",
+      background_color: "#fff",
+      theme_color: "#4d0",
+      display: "standalone",
+      icons: [
+        {
+          src: path.resolve('src/images/android/android-launchericon-512-512.png'),
+          sizes: [48, 72, 96, 144, 192, 512], // multiple sizes
+          purpose: 'any'
+        },
+        {
+          src: path.resolve('src/images/android/android-launchericon-192-192.png'),
+          size: '192x192',
+          purpose: 'maskable'
+        }
+      ]
     }),
     new CopyWebpackPlugin({
       patterns: [
@@ -45,3 +72,17 @@ module.exports = {
     port: 8080
   }
 };
+
+module.exports = (env, argv) => {
+  if (!env['WEBPACK_SERVE']) {
+    config.plugins.push(new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true,
+      maximumFileSizeToCacheInBytes: 5527705
+    }))
+  }
+
+  return config
+}
