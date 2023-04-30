8170b988a4f3c05486684a627b337970c8a23a2a
Учень12 <artems555666777@gmail.com>
2023-04-30 16:34:28 +0300

    'Update'

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import {App} from './app.js';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   window.app = new App();
 });
